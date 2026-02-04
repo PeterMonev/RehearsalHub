7cc87aa409b37362a8076a668e95faf42b4fd186
PeterMonev <monevpeter@gmail.com>
2026-02-04 12:49:05 +0200

    Add project files.

---
+++ ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RehearsalHub.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
