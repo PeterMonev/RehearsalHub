b153679557fd978082b8e527656e7ee57773a57b
PeterMonev <monevpeter@gmail.com>
2026-02-16 23:19:01 +0200

    implementing scaffold identity management

--- RehearsalHub/Program.cs
+++ RehearsalHub/Program.cs
@@ -26,7 +26,19 @@ namespace RehearsalHub
                 options.UseSqlServer(connectionString));
 
             builder.Services.AddDefaultIdentity<ApplicationUser>(options => {
-                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = true; 
+                options.Password.RequireLowercase = true; 
+                options.Password.RequireNonAlphanumeric = true; 
+                options.Password.RequireUppercase = true; 
+                options.Password.RequiredLength = 10; 
+                options.Password.RequiredUniqueChars = 1; 
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); 
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true; 
+                options.SignIn.RequireConfirmedAccount = false; 
             })
             .AddEntityFrameworkStores<ApplicationDbContext>();
 
